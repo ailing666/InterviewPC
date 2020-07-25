47d9bed2cdc35c19f86652d3b50d5d040e6237af
xatongxue <1120877019@qq.com>
2020-07-25 09:39:38 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,20 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // 注册
 Vue.use(VueRouter)
-
+// 导入子组件
+import Login from '@/views/login/Login.vue'
 // 实例化
 const router = new VueRouter({
-  routers: []
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
 })
 
 // 暴露
