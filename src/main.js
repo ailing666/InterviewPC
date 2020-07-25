99b57b27fb4589f4c1d2a79eceb91541acb8cc3a
xatongxue <1120877019@qq.com>
2020-07-25 09:24:29 +0800

    引入elementui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 // 导入router
 import router from '@/router/index.js'
+// 导入element-ui
+import ElementUi from 'element-ui'
+// d导入样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 注册
+Vue.use(ElementUi)
 Vue.config.productionTip = false
 
 new Vue({
